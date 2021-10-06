1548ec2dac89b77b644da2f082cd993bb18b0590
dshveigert <shveigert@gmail.com>
2021-10-06 15:08:11 +0700

    taiga-ui init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { TuiRootModule, TuiDialogModule, TuiNotificationsModule } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './landing/landing-page/landing-page.component';
 import { ChartPageComponent } from './chart/chart-page/chart-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -18,8 +19,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
-  ],
+    NoopAnimationsModule,
+      TuiRootModule,
+      BrowserAnimationsModule,
+      TuiDialogModule,
+      TuiNotificationsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
